4c3dc3b5bfd352b4eaeccb636850103d5b6ace27
Andruw Earnhardt <aearnhardt2000@gmail.com>
2019-07-01 07:17:27 +0100

    Transferred from Node.js to PHP

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'andruw');
+define('DB_PASSWORD', 'a16i14z12');
+define('DB_NAME', 'black-sheep');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
